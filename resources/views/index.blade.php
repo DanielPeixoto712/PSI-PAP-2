e88c16091937921c8385a80a8f808232d1922e77
anton <anton@DESKTOP-0RECEEQ.local>
2021-03-08 09:18:30 +0000

    Update 2jj.jpg and index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,7 +13,7 @@
 @section ('produtos')
  <div class="col-lg-9">
 
-        <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+        <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel" style="width: 930px">
           <ol class="carousel-indicators">
             <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
@@ -40,7 +40,45 @@
           </a>
         </div>
 
- 
+
+
+
+<div class="row">
+<div class="col-md-3 offset-md-1">
+          <div  class="card" style="width: 18rem;" >
+  <img class="card-img-top" src="img/omp2.jpg" alt="Imagem de capa do card">
+  <div class="card-body">
+    <h5 class="card-title">Temos vários volantes!</h5>
+    <p class="card-text">Para todos os gostos!</p>
+    <a href="{{route('omps.index')}}" target="blank" class="btn btn-primary">Visitar</a>
+  </div>
+</div>
+</div>
+
+<div class="col-md-3 offset-md-1">
+
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="img/omp2.jpg" alt="Imagem de capa do card">
+  <div class="card-body">
+    <h5 class="card-title">teste</h5>
+    <p class="card-text">trs</p>
+    <a href="https://www.google.pt/maps/place/Grandes+Planos+S.A./@41.3417066,-8.4791091,17z/data=!3m1!4b1!4m5!3m4!1s0xd24f58e776d1e03:0xda5ef49273bb8e1f!8m2!3d41.3417066!4d-8.4769204" target="blank" class="btn btn-primary">Visitar</a>
+  </div>
+</div>
+</div>
+
+<div class="col-md-3 offset-md-1">
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="img/2jj.jpg" alt="Imagem de capa do card">
+  <div class="card-body">
+    <h5 class="card-title">Aqui encontra o motor que procura!</h5>
+    <p class="card-text">Procure aqui!</p>
+    <a href="{{route('bmws.index')}}" target="blank" class="btn btn-primary">Visitar</a>
+  </div>
+</div>
+</div>
+</div> 
+
 
 
           
